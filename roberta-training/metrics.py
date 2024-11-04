a96bbb55c70f1820e12968bb1d24816b9146860e
Mark Hopkins <mh24@williams.edu>
2024-11-04 15:31:12 -0500

    Added some tests to metrics.

--- roberta-training/metrics.py
+++ roberta-training/metrics.py
@@ -21,12 +21,16 @@ def compute_bits_per_byte(model, tokenizer, eval_loader, device):
             outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=input_ids)
             logits = outputs.logits # shape: (batch_size, sequence_length, vocab_size)
 
+            print(logits)
+
             # Get log probs (cross entropy) for each token in sequence
             log_probs = cross_entropy(
-                logits.view(-1, model.config.vocab_size),
+                logits.view(-1, len(tokenizer)),
                 input_ids.view(-1),
                 reduction='none'
             ).view(input_ids.size())  # Reshape to (batch_size, sequence_length)
+
+            print(log_probs)
 
             # Divide by ln(2) to get bits per token, since cross_entropy uses ln
             # Shape: (batch_size,)
@@ -47,3 +51,56 @@ def compute_bits_per_byte(model, tokenizer, eval_loader, device):
     # Return average BPB over all batches
     return total_bpb / total_batches
 
+
+class SimpleTokenizer:
+    def __init__(self):
+        self.token_map = 'abcd'
+        self.pad_token_id = 4
+    
+    def __len__(self):
+        return 4
+    
+    def _decode_token(self, id):
+        return self.token_map[id]
+    
+    def decode(self, ids, skip_special_tokens=True):
+        return ' '.join([self._decode_token(tok) for tok in ids.tolist()])
+        
+
+class UnigramLMOutput:
+    
+    def __init__(self, logits):
+        self.logits = logits
+    
+    
+class UnigramLM(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+            
+    def forward(self, input_ids, attention_mask, labels):
+        
+        def get_probs(token):
+            return [0.8664,0.1732,-0.5199,-0.5199]  
+        
+        def map_probs(token_seq):
+            return [get_probs(tok) for tok in token_seq]
+        
+        logits = torch.tensor([map_probs(tok_seq) for tok_seq in input_ids.tolist()])
+        return UnigramLMOutput(logits)
+
+def my_eval_loader():
+    yield {'input_ids': torch.tensor([[0, 1, 0, 1, 2, 0, 0, 3],
+                                      [0, 1, 0, 1, 2, 0, 3, 0]])}
+
+if __name__ == "__main__":
+    input_ids = torch.tensor([[0, 1, 0, 1, 2, 0, 0, 3],
+                              [0, 1, 0, 1, 2, 0, 3, 0]])
+    labels = input_ids.clone()
+    attention_mask = torch.ones((2, 8))
+    model = UnigramLM()
+    output = model(input_ids, attention_mask, labels)
+    tokenizer = SimpleTokenizer()
+    print(tokenizer.decode(input_ids[0]))
+    eval_loader = my_eval_loader()
+    result = compute_bits_per_byte(model, tokenizer, eval_loader, device='cpu')
+    print(result)
